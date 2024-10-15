1ea7d0a2bdb97d88053c82b709dd37160a6efcea
aman.sharma2 <aman.sharma2@tothenew.com>
2024-10-16 00:10:07 +0530

    tailwind configured

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <header className='p-[15px] shadow-xl'>
+             <div className='max-w-[1200px] mx-auto border border-red-500'>
+                <div className='w-[100px]'>
+                    Tailwind Configured
+                </div>
+             </div>
+        </header> 
+    )
+
+}
+
+export default Header;
